382b6a39ac8d3765b70fe6fa4f1462677b3ee661
Sujay Gad <gadsujay6026@gmail.com>
2020-07-22 00:07:58 +0530

    c++ files

---
+++ triplets.cpp
@@ -0,0 +1,94 @@
+//
+//  triplets.cpp
+//  coding
+//
+//  Created by sujay2611 on 21/07/20.
+//  Copyright © 2020 sujay2611. All rights reserved.
+//
+
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the beautifulTriplets function below.
+int beautifulTriplets(int d, vector<int> arr) {
+    int c=0;
+    for(auto x:arr)
+    {
+        if(find(arr.begin(),arr.end(),x+d)!=arr.end() && find(arr.begin(),arr.end(),x+2*d)!=arr.end())
+        {
+            c+=1;
+        }
+
+    }
+    return c;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nd_temp;
+    getline(cin, nd_temp);
+
+    vector<string> nd = split_string(nd_temp);
+
+    int n = stoi(nd[0]);
+
+    int d = stoi(nd[1]);
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int result = beautifulTriplets(d, arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
